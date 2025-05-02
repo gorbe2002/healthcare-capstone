454f332b0c783cfc1d2bd6e36b078c33c4cdb878
Gabriel <95147025+gorbe2002@users.noreply.github.com>
2025-05-02 12:51:17 -0400

    Add files via upload

---
+++ data_pulls/CASTNET.py
@@ -0,0 +1,7 @@
+https://gaftp.epa.gov/castnet/CASTNET_Outgoing/data/ozone.zip
+
+https://gaftp.epa.gov/castnet/CASTNET_Outgoing/data/gas_calibration.zip
+
+https://gaftp.epa.gov/castnet/CASTNET_Outgoing/data/drychem.zip
+
+https://gaftp.epa.gov/castnet/CASTNET_Outgoing/data/metdata.zip
